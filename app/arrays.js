af0577332d13a44e76597b2fc7918e147bce20b7
vixus0 <vixus0@gmail.com>
2015-10-24 22:00:17 +0100

    Completed arrays and async tests

--- app/arrays.js
+++ app/arrays.js
@@ -3,58 +3,89 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum : function(arr) {
-
+    return arr.reduce(function(p, cv) {
+      return p + cv;
+    }, 0);
   },
 
   remove : function(arr, item) {
-
+    return _.filter(arr, function(n) { return n !== item; });
   },
 
   removeWithoutCopy : function(arr, item) {
+    var i;
 
+    while ( (i = arr.indexOf(item)) >= 0 ) {
+      arr.splice(i, 1);
+    }
+
+    return arr;
   },
 
   append : function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate : function(arr) {
-
+    arr.pop();
+    return arr;
   },
 
   prepend : function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
   curtail : function(arr) {
-
+    arr.shift();
+    return arr;
   },
 
   concat : function(arr1, arr2) {
-
+    return arr1.concat(arr2);
   },
 
   insert : function(arr, item, index) {
-
+    arr.splice(index, 0, item);
+    return arr;
   },
 
   count : function(arr, item) {
-
+    return arr.reduce(function(p, cv) {
+      return p + ((cv === item)? 1 : 0);
+    }, 0);
   },
 
   duplicates : function(arr) {
+    var 
+      count = [],
+      dups = [],
+      key;
 
+    _.each(arr, function (cv) {
+      if ( !count[cv] ) count[cv] = 0;
+      count[cv] += 1;
+    });
+
+    _.each(count, function (cv, i) {
+      if (cv > 1) dups.push(i);
+    });
+
+    return dups;
   },
 
   square : function(arr) {
-
+    return arr.map(function(cv) { return cv*cv; });
   },
 
   findAllOccurrences : function(arr, target) {
-
+    var idx = [];
+    arr.forEach(function (cv, i) { if(cv === target) idx.push(i); });
+    return idx;
   }
 };
--- app/async.js
+++ app/async.js
@@ -2,10 +2,24 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    return new Promise(function (resolve, reject) {
+          resolve(value);
+        });
   },
 
   manipulateRemoteData : function(url) {
+    return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
+      xhr.send();
+      xhr.addEventListener('load', function() {
+        var 
+          data = JSON.parse(xhr.responseText),
+          sorted = _.sortBy(data.people, 'name'),
+          names = _.pluck(sorted, 'name');
 
+        resolve(names);
+      });
+    });
   }
 };
