5f675da1b374f5226766773ac771a8d139438829
vixus0 <vixus0@gmail.com>
2015-10-25 21:04:32 +0000

    Completed objects tests

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,26 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.objectsAnswers =  {
   alterContext : function(fn, obj) {
-
+    return fn.call(obj);
   },
 
   alterObjects : function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate : function(obj) {
-
+    /*
+     * Alternative for compatibility
+     *
+     * var props = [];
+     * for (var k in obj) {
+     *   if (obj.hasOwnProperty(k)) {
+     *     props.push(k + ': ' + obj[k]);
+     *   }
+     * }
+     *
+     * return props;
+     */
+    return Object.keys(obj).map(function(v) { return v + ': '+ obj[v]; });
   }
 };
