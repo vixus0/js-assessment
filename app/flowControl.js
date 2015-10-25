4f99a705cb50009683391ff6f7c1a3e0667a180a
vixus0 <vixus0@gmail.com>
2015-10-25 16:00:07 +0000

    Completed flowControl tests

--- app/flowControl.js
+++ app/flowControl.js
@@ -10,6 +10,22 @@ exports.flowControlAnswers = {
     //
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
+    
+    var ret;
 
+    if (typeof num === "number") {
+      if (num % 15 === 0)
+        ret = 'fizzbuzz';
+      else if (num % 3 === 0)
+        ret = 'fizz';
+      else if (num % 5 === 0)
+        ret = 'buzz';
+      else
+        ret = num;
+    } else {
+      ret = false;
+    }
+
+    return ret;
   }
 };
