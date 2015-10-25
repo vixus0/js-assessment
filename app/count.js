899087baa71aed92bf540aa7fade99de47af1e8e
vixus0 <vixus0@gmail.com>
2015-10-25 15:59:57 +0000

    Counter tests seems impossible to complete (not sure console.log overriding works)

--- app/count.js
+++ app/count.js
@@ -2,6 +2,15 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.countAnswers =  {
   count : function (start, end) {
+    var timeout;
 
+    function go() {
+      console.log(start++);
+      if (start <= end) timeout = setTimeout(go, 100);
+    }
+
+    return {
+      cancel: function() { timeout && clearInterval(timeout); }
+    };
   }
 };
